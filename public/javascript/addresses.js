9f1230a7d0bb333c733d37249f7c93afdaf109c6
Kevin Kusmider <kevin.kusmider@hotmail.fr>
2022-06-18 22:40:11 +0200

    address corrections

--- public/javascript/addresses.js
+++ public/javascript/addresses.js
@@ -8,21 +8,14 @@ function toggleAddAddressForm(el) {
     form.classList.toggle("displayNone");
 }
 
-function changeFavAddress(id) {
+function changeFavAddress(el, id) {
     const request = new XMLHttpRequest();
     
     request.onreadystatechange = function() {
         if(request.readyState == 4){
-            console.log(request.response);
-            // if(request.responseText != "null" && parseInt(request.responseText) != NaN) {
-            //     if(parseInt(request.responseText) <= 0) {
-            //         element.parentElement.parentElement.remove();
-            //     } else {
-            //         element.parentElement.children[1].textContent = request.responseText
-            //         const prixUnite = element.parentElement.parentElement.children[2].textContent;
-            //         element.parentElement.parentElement.children[4].textContent = request.responseText * prixUnite;
-            //     }
-            // }
+            const radio = el.querySelector("input");
+            console.log(radio);
+            radio.checked = true;
         }
     }
     
